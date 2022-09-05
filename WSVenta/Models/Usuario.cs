5f1f9ff4d43a02e169d9bd8ae8f1b9d4d40c41d9
Jaime Gomez <gomezcos@gmail.com>
2022-09-04 23:43:12 -0600

    upload first

---
+++ WSVenta/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WSVenta.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ WSVenta/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVenta.Tools;
+using WSVenta.Models.Response;
+using WSVenta.Models.Request;
+using WSVenta.Models;
+
+namespace WSVenta.Services
+{
+    public class UserService : IUserService
+    {
+        public UserResponse Auth(AuthRequest model)
+        {
+            UserResponse userresponse = null;
+            using (var db = new VentaRealContext())
+            {
+                string spassword = Encrypt.GetSHA256(model.Password);
+                var usuario = db.Usuario.Where(d => d.Email == model.Email && d.Password == spassword).FirstOrDefault();
+
+                if (usuario == null) return null;
+                userresponse.Email = usuario.Email;
+            }
+
+            return userresponse;
+   
+        }
+    }
+}
